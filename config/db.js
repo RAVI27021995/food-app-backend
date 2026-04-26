6f69e448e32c6812ab0b69b025e1ac1aaa205df8
Ravindra Pagare <pagarerv21@gmail.com>
2026-04-27 00:00:20 +0530

    debug DATABASE_URL

--- config/db.js
+++ config/db.js
@@ -1,11 +1,20 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "food_app_db",
-  password: "Ravip@ssw0rd",
-  port: 5432,
-});
+console.log("DATABASE_URL:", process.env.DATABASE_URL); //temporary 
+const pool = new Pool(
+// {
+  // user: "postgres",
+  // host: "localhost",
+  // database: "food_app_db",
+  // password: "Ravip@ssw0rd",
+  // port: 5432,
+// }
+{
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+}
+);
 
 module.exports = pool;
